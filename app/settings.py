1fae2a394a470477ac9f9ddf498b12421515909c
danielerat <davidodo2015@gmail.com>
2023-04-10 18:29:59 +0200

    Adding Cors Headers to allow other ports and source to access our api endpoints

--- app/settings.py
+++ app/settings.py
@@ -31,12 +31,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'core',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -122,3 +124,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'core.User'
+
+# ALLOW ANY FRONT END TO ACCESS OUR PORT
+CORS_ORIGIN_ALLOW_ALL = True
+# ALlow Front end to get the refresh token in the cookie
+CORS_ALLOW_CREDENTIALS = True
