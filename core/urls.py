66f419c69452f521154604a30aebeb7a4fe7aa46
danielerat <davidodo2015@gmail.com>
2023-04-10 22:53:34 +0200

    Creating a token model for extra layer of protection to keep the logged in users in the database

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import RegisterAPIView, LoginAPIView, UserAPIView, RefreshAPIView
+from .views import LogoutAPIView, RegisterAPIView, LoginAPIView, UserAPIView, RefreshAPIView
 urlpatterns = [
     path("register/", RegisterAPIView.as_view(), name="register"),
     path("login/", LoginAPIView.as_view(), name="login"),
     path("user/", UserAPIView.as_view(), name="user"),
     path("refresh/", RefreshAPIView.as_view(), name="refresh"),
+    path("logout/", LogoutAPIView.as_view(), name="logout"),
 
 ]
